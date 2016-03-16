fdf02a2c85c698c601f0f2714295a57877694dd4
dabinlee708 <dabinlee708@users.noreply.github.com>
2016-03-16 11:08:23 +0800

    Update aircon.py
    Printouts include date & time of the credit balance update

--- aircon.py
+++ aircon.py
@@ -3,9 +3,9 @@ from bs4 import BeautifulSoup
 
 credit_wallet="https://nus-utown.evs.com.sg/SUTDMain/viewMeterCreditServlet"
 login="https://nus-utown.evs.com.sg/SUTDMain/loginServlet"
-log_val =dict(
-				txtLoginId = 'aircon unit ID',
-				txtPassword = 'PW')
+log_val = dict(
+				txtLoginId ='20000596',
+				txtPassword='chwb5278')
 s=requests.session()
 r=s.post(login,data=log_val)
 r=s.get(credit_wallet)
@@ -14,4 +14,4 @@ soup = BeautifulSoup(html,'lxml')
 parsed_list=[]
 for td_tag in soup.find_all('font'):
 	parsed_list.append(td_tag.text)
-print "Meter ID: ",parsed_list[2],"\nCredit: ",parsed_list[5]
+print "Meter ID: ",parsed_list[2],"\nCredit  : ",parsed_list[5], "\nTime    : ",parsed_list[6]
